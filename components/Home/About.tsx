824cd6a9be38eae38d53841ceb06c0ce6705feb8
selim121 <selimkhan.bdkhan@gmail.com>
2025-04-08 12:42:09 +0600

    about us updated

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -4,9 +4,9 @@ import HeadingTwo from '../reusable/HeadingTwo';
 const About = () => {
     return (
         <div className='max-w-[1352px] px-4 py-10 md:py-20 mx-auto'>
-            <div className="flex items-start justify-between gap-6">
+            <div className="flex flex-col lg:flex-row items-start justify-between gap-6">
                 <div className="w-full lg:w-[66%] flex flex-col gap-6">
-                    <div className="flex items-start justify-between gap-6">
+                    <div className="flex flex-col sm:flex-row items-start justify-between gap-6">
                         <div className="w-full lg:w-[52%]">
                             <img className='w-full h-full object-cover' src="/images/hs.png" alt="Hill place" />
                         </div>
@@ -16,24 +16,24 @@ const About = () => {
                         </div>
                     </div>
                     <div className="w-full h-[1px] bg-[#FEE9E9]"></div>
-                    <div className="flex items-center justify-between pr-16">
+                    <div className="flex flex-wrap items-center justify-between gap-6 pr-16">
                         <div className="flex flex-col items-start gap-2">
                             <div className="countNumber text-[48px] font-bold leading-[120%] text-[#121212]">2000+</div>
                             <p className="text-[16px] font-normal leading-[160%] text-[#4A4A4A] tracking-[0.5px]">Tourist have visited</p>
                         </div>
-                        <div className="w-[1px] h-[80px] bg-[#FEE9E9]"></div>
+                        <div className="hidden sm:block w-[1px] h-[80px] bg-[#FEE9E9]"></div>
                         <div className="flex flex-col items-start gap-2">
                             <div className="countNumber text-[48px] font-bold leading-[120%] text-[#121212]">576+</div>
                             <p className="text-[16px] font-normal leading-[160%] text-[#4A4A4A] tracking-[0.5px]">Tourist have visited</p>
                         </div>
-                        <div className="w-[1px] h-[80px] bg-[#FEE9E9]"></div>
+                        <div className="hidden sm:block w-[1px] h-[80px] bg-[#FEE9E9]"></div>
                         <div className="flex flex-col items-start gap-2">
                             <div className="countNumber text-[48px] font-bold leading-[120%] text-[#121212]">654+</div>
                             <p className="text-[16px] font-normal leading-[160%] text-[#4A4A4A] tracking-[0.5px]">Tourist have visited</p>
                         </div>
                     </div>
                 </div>
-                <div className="w-full lg:w-[32%]">
+                <div className="w-2/3 lg:w-[32%] mx-auto">
                     <img className='w-full h-full object-cover' src="/images/hill.png" alt="Hill place" />
                 </div>
             </div>
